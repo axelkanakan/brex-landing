d090515118ec0a920da3f677eec54bdd867299d2
Axel Kanakan <okulby@yahoo.com>
2019-08-10 12:28:15 -0700

    Card Component props

---
+++ src/Components/StartCard/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Outbutton from '../Outbutton';
+import './StartCard.css';
+
+
+const StartCard = ({title, content, image}) =>
+{
+    
+    return ( <div className= "StartCard">
+        <div className="card-container">
+            <div className="start-image">
+                <img src={image} alt="sign-up"/>
+            </div>
+            <div className="card-content">
+                <h3>{title}</h3>
+                <p>{content}</p>
+                <Outbutton />
+            </div>
+        </div>
+    </div>)}
+export default StartCard;
--- src/Containers/Start/index.js
+++ src/Containers/Start/index.js
@@ -1,12 +1,23 @@
 import React, {Component} from 'react';
 import './Start.css';
+import StartCard from '../../Components/StartCard';
+import cardImg from '../../Assets/card-img.png';
 
 class Start extends Component{
-    
+
+    state = {
+        card : {
+            title:'Easy online signup',
+            content:"80% of founders don't realize they are liable for their corporate card- we offer the only corporate card for startups.",
+            image: cardImg
+        }
+    }
     render(){
+        const {title, content, image} = this.state.card;
         return (
             <div className="Start">
                 <h1>Easy To Start</h1>
+                <StartCard title={title} content={content} image={image}/>
             </div>
         )
     }
