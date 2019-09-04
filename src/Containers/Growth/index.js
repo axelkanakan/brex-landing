fbe22993c907671544f4b62b506517aae6bb9ca5
Axel Kanakan <okulby@yahoo.com>
2019-09-04 11:31:03 -0700

    Growth Section 10%

---
+++ src/Containers/Growth/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Growth.css';
+import sectionImage from '../../Assets/sectionImageOne.png';
+
+const Growth = () => 
+    <div className="Growth">
+        <div className="Growth-title">
+        <h1> Great For Growth</h1>
+        <p> Startups don't remain startups from long. The Brex card comes with data and expense managament tools to control and manage the spending of scaling enterprise.</p>
+        </div>
+        <div className="g-content">
+            <div className="g-image">
+                <img src={sectionImage} alt="sectionOne"/>
+            </div>
+            <div className="g-description">
+                <h1>Full Control</h1>
+                <p>Use the dashboard to instantly add cardholders and set individual limits based on your business needs.</p>
+                <p>Learn More -></p>
+            </div>
+        </div>
+    </div>
+
+
+export default Growth;
--- src/Containers/Layout/index.js
+++ src/Containers/Layout/index.js
@@ -3,10 +3,12 @@ import React from 'react';
 import Hero from '../../Containers/Hero';
 import Footer from '../../Components/Footer';
 import Start from '../Start';
+import Growth from '../Growth';
 const Layout = ()=> (
     <div>
         <Hero/>
         <Start/>
+        <Growth/>
         <Footer/>
     </div>
 )
