120eb087564a2bd6ce08cc95e78d2f6ea73e9474
Axel Kanakan <okulby@yahoo.com>
2019-09-23 07:43:54 -0700

    Updated Data

--- src/Data/data.js
+++ src/Data/data.js
@@ -6,23 +6,23 @@ import cardImg3 from '../Assets/card-img3.png';
 export const cardData = {
     cardOne: {
         id: "cardOne",
-        title:"No Personal Liability",
-        content:"80% of founders don't realize they are liable for their corporate card- we offer the only corporate card for startups.",
+        title:"Easy online signup",
+        content:"Once approved you can immediately use your virtual card to purchase",
         image: cardImg2,
         onHover:false
     },
     cardTwo: {
         id:"cardTwo",
         title:"No Personal Liability",
-        content:"80% of founders don't realize they are liable for their corporate card- we offer the only corporate card for startups.",
+        content:"80% of founders don't realize they are liable for their corporate card- get the only card for start-ups.",
         image: cardImg,
         onHover:false
     },
 
     cardThree: {
         id:"cardThree",
-        title:"No Personal Liability",
-        content:"80% of founders don't realize they are liable for their corporate card- we offer the only corporate card for startups.",
+        title:"Higher Limits",
+        content:"Limits are typically 10-20 times higher than those of competitive cards",
         image: cardImg3,
         onHover:false
     }
